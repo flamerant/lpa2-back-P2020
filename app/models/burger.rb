f0004382c2abb3bb2be486112a679b8d25b4b113
Felicie Lamerant <flameran@utbm.fr>
2020-03-10 14:57:19 +0100

    ajout de la fonction nutriments

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,8 +1,12 @@
 class Burger < ApplicationRecord
     def nutriments
         #recupérer les données de l'API faire un tableau
-        product = Openfoodfacts::Product.get(self.code, locale: 'fr')
-        nutri = product.nutriments.to_hash
-        return nutri
+        if(!self.code.nil?)
+            product = Openfoodfacts::Product.get(self.code, locale: 'fr')
+            if(!self.product.nil?)
+                nutri = product.nutriments.to_hash
+                return nutri
+            end
+        end
     end
 end
