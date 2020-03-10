ecf2059ac2650cd97ffd5106733d319a8a78a8c8
félicie lamerant <lamerant.felicie@gmail.com>
2020-03-10 18:54:37 +0100

    methode nutriment

--- app/models/burger.rb
+++ app/models/burger.rb
@@ -1,13 +1,15 @@
 class Burger < ApplicationRecord
     def nutriments
-        #recupérer les données de l'API faire un tableau
-        if(!self.code.nil?)
-            product = Openfoodfacts::Product.get(self.code, locale: 'fr')
-            if(!self.product.nil?)
-                nutri = product.nutriments.to_hash
-                return nutri[0]
-            end
+        require 'openfoodfacts'
+    
+        product = Openfoodfacts::Product.get(self.code, locale: 'fr')
+        
+        if product == nil
+          product = [["No Data"], [""]]
+        else
+          product = product.nutriments
         end
-        return "Aucun"
-    end
+        
+        return product
+      end
 end
